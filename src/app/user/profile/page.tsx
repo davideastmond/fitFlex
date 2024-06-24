3fb14a159bd52a91d8bf44b09c8d7f9c150ab0b6
David Eastmond <davebuck1981@gmail.com>
2024-06-24 11:38:02 -0400

    add logic to allow user to update their password

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { UserClient } from "@/app/clients/user-client/user-client";
+import { passwordValidator } from "@/app/validators/password/password.validator";
+import { extractValidationErrors } from "@/app/validators/utils/extract-validation-errors/extract-validation-errors";
 import { BasicRoundedButton } from "@/components/buttons/basic-rounded-button/Basic-rounded-button";
 import SuccessModal from "@/components/modals/SuccessModal";
 import { useAuthSession } from "@/lib/contexts/auth-context/auth-context";
@@ -16,6 +18,8 @@ export default function ProfilePage() {
   const [userNameFieldValue, setUserNameFieldValue] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [successModalOpen, setSuccessModalOpen] = useState<boolean>(false);
+  const [password1FieldValue, setPassword1FieldValue] = useState<string>("");
+  const [password2FieldValue, setPassword2FieldValue] = useState<string>("");
   const router = useRouter();
 
   useEffect(() => {
@@ -23,18 +27,54 @@ export default function ProfilePage() {
   }, [session?.user?.username]);
 
   const handleSaveUserInfo = async () => {
+    let fieldsToUpdate: Record<string, string> = {};
+
     setErrorMessage(null);
     if (!userNameFieldValue || userNameFieldValue.trim() === "") {
       setErrorMessage("The username field can't be empty.");
       return;
     }
+
+    if (
+      password1FieldValue.trim().length > 0 ||
+      password2FieldValue.trim().length > 0
+    ) {
+      try {
+        await passwordValidator.validate(
+          {
+            password1: password1FieldValue,
+            password2: password2FieldValue,
+          },
+          { abortEarly: false }
+        );
+        fieldsToUpdate = {
+          password: password1FieldValue,
+        };
+      } catch (error: any) {
+        const passwordErrors = extractValidationErrors(error);
+        setErrorMessage(
+          Object.values(passwordErrors)
+            .map((error) => error.message)
+            .join(" ")
+        );
+        return;
+      }
+    }
+
     setIsBusy(true);
+    fieldsToUpdate = {
+      ...fieldsToUpdate,
+      username: userNameFieldValue,
+    };
+
     try {
-      await UserClient.updateUserInfo({ username: userNameFieldValue });
+      await UserClient.updateUserInfo(fieldsToUpdate);
       setSuccessModalOpen(true);
       if (update) {
         update();
       }
+      setPassword1FieldValue("");
+      setPassword2FieldValue("");
     } catch (error: any) {
       setErrorMessage(error.message);
     } finally {
@@ -66,25 +106,63 @@ export default function ProfilePage() {
         </Link>
       </motion.div>
       <div className="w-full">
-        <div>
+        {session?.user?.oAuth && (
+          <div>
+            <h1 className="mt-4 text-center text-md  openSansFont self-center text-blue-600">
+              Note: these values are not editable because you signed with a 3rd
+              party provider.
+            </h1>
+          </div>
+        )}
+        <div className="mt-4">
+          <h1 className="text-lg mb-2 text-stone-500">My E-mail Address</h1>
+          <p className="p-2 bg-gray-50 w-full robotoFont h-16 text-xl hover:bg-neutral-300 content-center">
+            {session?.user?.email}
+          </p>
+        </div>
+        <div className="mt-8">
           <h1 className="text-lg mb-2 text-stone-500">My Name</h1>
           <input
             type="text"
-            id="templateName"
+            id="username"
             maxLength={50}
             autoComplete="off"
             value={userNameFieldValue}
             onChange={(e) => setUserNameFieldValue(e.target.value)}
             placeholder="Enter a user name"
             className="p-2 bg-gray-50 w-full robotoFont h-16 text-xl hover:bg-neutral-300"
-            disabled={isBusy}
+            disabled={isBusy || session?.user?.oAuth}
           />
         </div>
-        <div className="mt-2">
-          <h1 className="text-lg mb-2 text-stone-500">My E-mail Address</h1>
-          <p className="p-2 bg-gray-50 w-full robotoFont h-16 text-xl hover:bg-neutral-300 content-center">
-            {session?.user?.email}
-          </p>
+        {/* Section for user to update their password 
+          this section is only visible for credentialed users (non-google oAuth)
+        */}
+        <div className="mt-8">
+          <h1 className="text-lg mb-2 text-stone-500">Update Password</h1>
+          <div className="flex flex-col gap-2">
+            <input
+              type="password"
+              id="password1"
+              maxLength={50}
+              autoComplete="off"
+              value={password1FieldValue}
+              onChange={(e) => setPassword1FieldValue(e.target.value)}
+              placeholder="Enter a new password"
+              className="p-2 bg-gray-50 w-full robotoFont h-16 text-xl hover:bg-neutral-300"
+              disabled={isBusy || session?.user?.oAuth}
+            />
+            <input
+              type="password"
+              id="password2"
+              maxLength={50}
+              autoComplete="off"
+              value={password2FieldValue}
+              onChange={(e) => setPassword2FieldValue(e.target.value)}
+              placeholder="Confirm new password"
+              className="p-2 bg-gray-50 w-full robotoFont h-16 text-xl hover:bg-neutral-300"
+              disabled={isBusy || session?.user?.oAuth}
+            />
+          </div>
         </div>
       </div>
       <div>
@@ -102,8 +180,10 @@ export default function ProfilePage() {
             buttonClassNames="defaultButtonColor !justify-evenly"
             disabled={
               isBusy ||
+              session?.user?.oAuth ||
               (session?.user?.username === userNameFieldValue &&
-                userNameFieldValue?.trim() !== "")
+                userNameFieldValue?.trim() !== "" &&
+                password1FieldValue.trim() !== password2FieldValue.trim())
             }
           >
             {isBusy && <CircularProgress size={30} sx={{ color: "white" }} />}
